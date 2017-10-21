a98430fe2944cca8b7562c3db43f267c38c5d34f
nogalavi <noga@indeni.com>
2017-10-21 20:37:54 +0300

    started adding content

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Our Initial Commit!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <p>Netlify is Awesome!</p>
-      </div>
+        <div className="App-header">
+          <h1 dir="rtl" className="App-title">קרחי העולם התפקדו!</h1>
+        </div>
+          <p dir="rtl">אהלן חברים קרחים ומזוקנים שלי.
+              קוראים לי עמית שמעוני ואני אמן בוגר בצלאל.
+              לינק לאתר.
+          </p>
+          <p dir="rtl">אני עובד על פרוייקט חדש ונחמד שכולו קרחות וזקנים.
+              בעקרון מדובר בפוייקט צילום פורטרטים, אבל בגלל שכולנו כאלה מדהימים יש מצב שעל הדרך נשתלט על העולם.
+              אז אם גם אתם, כמוני, החלטתם לטפח את הצד החזק שלכם (ז״א שיש לכם קרחת וזקן) אני ממש אשמח שתשתתפו בפרוייקט. בינתיים אתם רק צריכים להירשם ובקרוב ממש תקבלו עידכונים.</p>
+          <p dir="rtl">רעיונות יתקבלו בברכה!
+          </p>
+          <p dir="rtl">*הפנייה מנוסחת בלשון זכר אבל פונה רק לגברים עם קרחת וזקן
+          </p>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSelMsjwb02yynL5EeBKGLUEevpkByEmGVmzfKIuVhNw3pCzzw/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>      </div>
     );
   }
 }
